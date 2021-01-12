2ea86952dce617f2cc0bcc98c4654e984ef7a959
santiagoCeballos <santiago.bike95@gmail.com>
2021-01-11 23:31:21 -0500

    sc modularizando esquema grapql

---
+++ db.js
@@ -0,0 +1,24 @@
+'use stric'
+
+const { MongoClient } = require('mongodb')
+
+class Conexion {
+    db
+    constructor(){
+        this.mongoConnection()
+    }
+
+    mongoConnection() {
+        MongoClient.connect(process.env.CONNECTION_DB, 
+            {   
+                useNewUrlParser: true, 
+                useUnifiedTopology: true  
+            }
+        ).then(client => { 
+            this.db = client.db('code-rockDB'), 
+            console.log('Data base live') 
+        }).catch(err => console.log(`Error data base`, err))
+    }
+}
+
+module.exports = new Conexion().db
